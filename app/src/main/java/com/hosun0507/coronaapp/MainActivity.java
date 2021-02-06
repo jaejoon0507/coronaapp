7d693f773f8927ae2eaf7bba1c4a1057028e3049
재준 정 <hosun0507@gmail.com>
2021-02-07 08:58:00 +0900

    first start

---
+++ app/src/main/java/com/hosun0507/coronaapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hosun0507.coronaapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
