5f8088c4204884f7c755f76a0f529486fb0f06de
재준 정 <hosun0507@gmail.com>
2021-02-08 00:43:21 +0900

    뷰 바인딩 설정

--- app/src/main/java/com/hosun0507/coronaapp/MainActivity.java
+++ app/src/main/java/com/hosun0507/coronaapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.hosun0507.coronaapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hosun0507.coronaapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    ActivityMainBinding activityMainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+    }
+    public void button1 (View view){
+        activityMainBinding.button1.setText("하잉");
     }
 }
